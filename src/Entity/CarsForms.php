68237439d7fdb67b91aaf064771d800d5972e410
seylionel <seymlionel@gmail.com>
2020-09-29 13:31:41 +0200

    Ajout de colonne dans le formulaire auto

--- src/Entity/CarsForms.php
+++ src/Entity/CarsForms.php
@@ -22,6 +22,42 @@ class CarsForms
      */
     private $modele;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $power;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $gas;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $registration;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $has_insurance;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=prospect::class, inversedBy="carsForms")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $prospects;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +71,90 @@ class CarsForms
     public function setModele(string $modele): self
     {
         $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getPower(): ?int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): self
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    public function getGas(): ?string
+    {
+        return $this->gas;
+    }
+
+    public function setGas(string $gas): self
+    {
+        $this->gas = $gas;
+
+        return $this;
+    }
+
+    public function getRegistration(): ?string
+    {
+        return $this->registration;
+    }
+
+    public function setRegistration(string $registration): self
+    {
+        $this->registration = $registration;
+
+        return $this;
+    }
+
+    public function getHasInsurance(): ?bool
+    {
+        return $this->has_insurance;
+    }
+
+    public function setHasInsurance(bool $has_insurance): self
+    {
+        $this->has_insurance = $has_insurance;
+
+        return $this;
+    }
+
+    public function getProspects(): ?prospect
+    {
+        return $this->prospects;
+    }
+
+    public function setProspects(?prospect $prospects): self
+    {
+        $this->prospects = $prospects;
 
         return $this;
     }
--- src/Entity/Prospect.php
+++ src/Entity/Prospect.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProspectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Prospect
      */
     private $lastname;
 
+    /**
+     * @ORM\OneToMany(targetEntity=CarsForms::class, mappedBy="prospects")
+     */
+    private $carsForms;
+
+    public function __construct()
+    {
+        $this->carsForms = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,37 @@ class Prospect
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|CarsForms[]
+     */
+    public function getCarsForms(): Collection
+    {
+        return $this->carsForms;
+    }
+
+    public function addCarsForm(CarsForms $carsForm): self
+    {
+        if (!$this->carsForms->contains($carsForm)) {
+            $this->carsForms[] = $carsForm;
+            $carsForm->setProspects($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCarsForm(CarsForms $carsForm): self
+    {
+        if ($this->carsForms->contains($carsForm)) {
+            $this->carsForms->removeElement($carsForm);
+            // set the owning side to null (unless already changed)
+            if ($carsForm->getProspects() === $this) {
+                $carsForm->setProspects(null);
+            }
+        }
 
         return $this;
     }
